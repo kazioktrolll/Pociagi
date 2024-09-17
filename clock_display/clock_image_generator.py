18bd5e3fc6356743afcfcd588cd1b390feae2f2c
Kazioktroll <03woj.kwi03@gmail.com>
2024-09-17 19:29:23 +0200

    simplified clock_image_generator.py

--- clock_display/clock_image_generator.py
+++ clock_display/clock_image_generator.py
@@ -1,8 +1,8 @@
-from math import radians, sin, cos
-
 from kivy.app import App
-from kivy.graphics import Color, Ellipse, PushMatrix, Rectangle, Rotate, PopMatrix
+from kivy.graphics import Color, Ellipse
 from kivy.uix.widget import Widget
+
+from utensils import create_rotated_rectangle
 
 
 class MyWidget(Widget):
@@ -21,49 +21,19 @@ class MyWidget(Widget):
         with self.canvas:
             Color(0, 0, 0, 1)
 
-            center_x, center_y = center
             radius = 400
             rect_width = 30
             rect_height = 100
             for i in range(12):
                 angle_deg = i * 30  # Each strip is 30 degrees apart
-                angle_rad = radians(angle_deg)
+                create_rotated_rectangle(angle_deg, rect_width, rect_height, radius, center)
 
-                # Calculate rectangle position along the circle
-                rect_x = center_x + radius * cos(angle_rad) - rect_width / 2
-                rect_y = center_y + radius * sin(angle_rad) - rect_height / 2
-
-                # Apply rotation to face towards the center
-                PushMatrix()
-                Rotate(angle=angle_deg + 90,
-                       origin=(rect_x + rect_width / 2, rect_y + rect_height / 2))  # Rotate rectangle
-
-                # Draw the rectangle
-                Rectangle(pos=(rect_x, rect_y), size=(rect_width, rect_height))
-
-                PopMatrix()  # Restore transformation matrix
-
-            center_x, center_y = center
             radius = 400
             rect_width = 10
             rect_height = 40
             for i in range(60):
-                angle_deg = i * 6  # Each strip is 30 degrees apart
-                angle_rad = radians(angle_deg)
-
-                # Calculate rectangle position along the circle
-                rect_x = center_x + radius * cos(angle_rad) - rect_width / 2
-                rect_y = center_y + radius * sin(angle_rad) - rect_height / 2
-
-                # Apply rotation to face towards the center
-                PushMatrix()
-                Rotate(angle=angle_deg + 90,
-                       origin=(rect_x + rect_width / 2, rect_y + rect_height / 2))  # Rotate rectangle
-
-                # Draw the rectangle
-                Rectangle(pos=(rect_x, rect_y), size=(rect_width, rect_height))
-
-                PopMatrix()  # Restore transformation matrix
+                angle_deg = i * 6  # Each strip is 6 degrees apart
+                create_rotated_rectangle(angle_deg, rect_width, rect_height, radius, center)
 
         self.export_to_png("../images/clock_image.png")
 
